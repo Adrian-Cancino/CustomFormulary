89ab732ed5b82c299d5c064874c660cfe8252fac
Adrian Cancino <71229190+Adrian-Cancino@users.noreply.github.com>
2023-05-09 20:55:32 -0600

    Login / Register / Logout and protected routes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom Login / Register 
+
+Create a formulare for Register and Login users. In this project protect routes only when the user is logged in.
---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class AuthManager extends Controller
+{
+    // Logic for Login and Registration
+    function login(){
+        if(Auth::check()){
+            return redirect()->intended(route('home'));
+        }
+        return view('login');
+    }
+
+    function loginPost(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if(Auth::attempt($credentials)){
+            return redirect()->intended(route('home'));
+        }
+        return redirect(route('login'))->with("error", "Login failed");
+    }
+
+    function register(){
+        return view('register');
+    }
+
+    function registerPost(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+
+        $user = User::create($data);
+        if(!$user){
+            return redirect(route('register'))->with("error", "Register failed");
+        }
+
+        return redirect(route('login'))->with("success", "Register success, Login to access");
+        
+    }
+
+    function logOut(){
+        Session::flush();
+        Auth::logout();
+        return redirect(route('login'));
+    }
+}
